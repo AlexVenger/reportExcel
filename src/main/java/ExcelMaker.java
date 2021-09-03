ce52de10bd04c6769fe548b3542abbdf656379cb
Igor <alexvenger456@gmail.com>
2021-09-03 17:07:59 +0400

    Styles replaced into separate class

--- src/main/java/ExcelMaker.java
+++ src/main/java/ExcelMaker.java
@@ -11,12 +11,12 @@ import java.io.IOException;
 import java.util.*;
 
 public class ExcelMaker {
-    private static Map<String, CellStyle> styles;
     private static SXSSFSheet sheet;
+    private static Map<String, CellStyle> styles = new HashMap<>();
 
     public static byte[] createExcel(List<String> headers, List<List<Object>> bodies) {
         SXSSFWorkbook workbook = new SXSSFWorkbook();
-        createStyles(workbook);
+        ExcelStyles.createStyles(workbook, styles);
 
         List<Object> body;
 
@@ -47,105 +47,5 @@ public class ExcelMaker {
         }
 
         return fileOut.toByteArray();
-    }
-
-    private static void createStyles(Workbook workbook) {
-        styles = new HashMap<>();
-
-        CellStyle headerCellStyle = workbook.createCellStyle();
-
-        Font headerFont = workbook.createFont();
-        headerFont.setFontHeightInPoints((short) 24);
-        headerFont.setFontName("Arial");
-
-
-        headerFont.setColor(IndexedColors.BLACK.getIndex());
-
-        headerFont.setBold(true);
-        headerFont.setItalic(false);
-        headerCellStyle.setFont(headerFont);
-
-
-        styles.put("style1", headerCellStyle);
-
-
-
-        XSSFCellStyle titleCellStyle = (XSSFCellStyle) workbook.createCellStyle();
-        Font titleFont = workbook.createFont();
-        titleFont.setFontName("Arial");
-        titleFont.setColor(IndexedColors.BLACK.getIndex());
-        titleFont.setBold(true);
-        titleCellStyle.setFillForegroundColor(new XSSFColor(new java.awt.Color(220, 230, 241)));
-        titleCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-        titleCellStyle.setFont(titleFont);
-        titleCellStyle.setBorderBottom(BorderStyle.MEDIUM);
-        titleCellStyle.setBorderTop(BorderStyle.MEDIUM);
-        titleCellStyle.setBorderRight(BorderStyle.MEDIUM);
-        titleCellStyle.setBorderLeft(BorderStyle.MEDIUM);
-
-        styles.put("style2", titleCellStyle);
-
-        CellStyle dataStyle = workbook.createCellStyle();
-        Font dataFont = workbook.createFont();
-        dataFont.setFontName("Arial");
-        dataFont.setColor(IndexedColors.BLACK.getIndex());
-        dataStyle.setFont(dataFont);
-        dataStyle.setBorderBottom(BorderStyle.MEDIUM);
-        dataStyle.setBorderTop(BorderStyle.MEDIUM);
-        dataStyle.setBorderRight(BorderStyle.MEDIUM);
-        dataStyle.setBorderLeft(BorderStyle.MEDIUM);
-
-        styles.put("style3", dataStyle);
-
-
-        CellStyle newStyle = workbook.createCellStyle();
-        newStyle.setWrapText(true);
-
-        newStyle.setAlignment(HorizontalAlignment.CENTER);
-        newStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-        Font newFont = workbook.createFont();
-        newFont.setFontHeightInPoints((short) 11);
-        newFont.setFontName("Arial");
-        newFont.setColor(IndexedColors.BLACK.getIndex());
-        newStyle.setFont(newFont);
-        styles.put("style4", newStyle);
-
-        CellStyle newStyle1 = workbook.createCellStyle();
-        newStyle1.setWrapText(true);
-        newStyle1.setAlignment(HorizontalAlignment.CENTER);
-        newStyle1.setVerticalAlignment(VerticalAlignment.CENTER);
-        Font newFont1 = workbook.createFont();
-        newFont1.setFontHeightInPoints((short) 18);
-        newFont1.setFontName("Arial");
-        newFont1.setColor(IndexedColors.BLACK.getIndex());
-        newFont1.setBold(true);
-        newStyle1.setFont(newFont1);
-        styles.put("style5", newStyle1);
-
-        CellStyle sumStyle = workbook.createCellStyle();
-        sumStyle.setWrapText(true);
-        sumStyle.setAlignment(HorizontalAlignment.CENTER);
-        sumStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-        Font sumFont = workbook.createFont();
-        sumFont.setFontHeightInPoints((short) 12);
-        sumFont.setFontName("Arial");
-        sumFont.setColor(IndexedColors.BLACK.getIndex());
-        sumFont.setBold(true);
-        sumStyle.setFont(sumFont);
-        sumStyle.setBorderTop(BorderStyle.MEDIUM);
-        styles.put("style6", sumStyle);
-
-
-        CellStyle newStyleDoubles = workbook.createCellStyle();
-        newStyleDoubles.setWrapText(true);
-
-        newStyleDoubles.setAlignment(HorizontalAlignment.CENTER);
-        newStyleDoubles.setVerticalAlignment(VerticalAlignment.CENTER);
-        Font newFontDoubles = workbook.createFont();
-        newFontDoubles.setFontHeightInPoints((short) 11);
-        newFontDoubles.setFontName("Arial");
-        newFontDoubles.setColor(IndexedColors.BLACK.getIndex());
-        newStyleDoubles.setFont(newFontDoubles);
-        styles.put("style7", newStyleDoubles);
     }
 }
