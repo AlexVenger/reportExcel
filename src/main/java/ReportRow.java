7a2439f4e9efc5acc1d7bbff49daa769be12014e
Igor <alexvenger456@gmail.com>
2021-09-03 17:00:27 +0400

    ReportRow classes deleted

--- src/main/java/ReportRow.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public interface ReportRow {
-    ArrayList<Object> body = new ArrayList<>();
-
-    ArrayList<Object> getBody();
-
-    void setBody(ArrayList<Object> body);
-}
